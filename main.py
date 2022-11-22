532b35d7ec78346c62a2c9d8637abc596be8ec11
Augusto Fadanelli de Sousa <augustofadanelli@protonmail.com>
2022-11-22 16:57:50 -0300

    Foi criado o sistema que gera os mapas a partir de uma matriz.

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ import numpy as np
 
 
 class StartGame:
-    def __init__(self, map:np.matrix):
+    def __init__(self, map:np.matrix, theme:str):
         self.map = map
+        self.theme = theme
 
-        self.__screen_width = 600
-        self.__screen_height = 400
+        self.__screen_width = 960
+        self.__screen_height = 832
         self.__screen = pygame.display.set_mode((self.__screen_width, self.__screen_height))
 
         self.__clock = pygame.time.Clock()
 
         pygame.display.set_caption('Bomber-Man')
 
-        self.__map_generate()
+        # LOAD ASSETS
+        self.assets = []
+        self.__load_assets()
+
         self.__loop()
 
     def __loop(self):
@@ -29,31 +33,59 @@ class StartGame:
                     pygame.quit()
                     exit()
 
+            self.__map_generate()
             pygame.display.update()
 
     def __map_generate(self):
-        for line in range(4):
-            for column in range(6):
-                print(self.map[line, column])
-                match self.map[line, column]:
-                    case 1:
-                        print('red')
-                    case 2:
-                        print('green')
-                    case 3:
-                        print('blue')
-                    case 4:
-                        print('yellow')
+        for line in range(13):
+            for column in range(15):
+                x = (column + 1) * 64
+                y = (line + 1) * 64
+                if(line < 11 and column < 13):
+                    match self.map[line, column]:
+                        case 'GR':
+                            self.__screen.blit(self.assets[0], (x, y))
+                        case 'BG':
+                            self.__screen.blit(self.assets[1], (x, y))
+                        case 'BA':
+                            self.__screen.blit(self.assets[2], (x, y))
+                        case 'BO':
+                            self.__screen.blit(self.assets[3], (x, y))
+                if(column == 0 or column == 14):
+                    self.__screen.blit(self.assets[4], (column * 64, line * 64))
+                elif(line == 0 or line == 12):
+                    self.__screen.blit(self.assets[5], (column * 64, line * 64))
 
 
-if __name__ == '__main__': 
-    map = np.matrix([[1, 2, 3, 4, 1, 2], 
-                     [4, 3, 2, 1, 2, 3],
-                     [2, 1, 3, 4, 3, 1],
-                     [3, 2, 1, 2, 4, 3]])
+    def __load_assets(self):
+        self.assets.append(pygame.image.load(f'assets/{self.theme}/ground.jpg'))
+        self.assets.append(pygame.image.load(f'assets/{self.theme}/burnt_ground.jpg'))
+        self.assets.append(pygame.image.load(f'assets/{self.theme}/barricade.jpg'))
+        self.assets.append(pygame.image.load(f'assets/{self.theme}/box.jpg'))
+        self.assets.append(pygame.image.load(f'assets/{self.theme}/side_wall.jpg'))
+        self.assets.append(pygame.image.load(f'assets/{self.theme}/vertical_wall.jpg'))
+
+
+if __name__ == '__main__':
+    '''
+    GR - ground
+    BG - burnt ground
+    BA - barricade
+    BO - box
+    '''
+    # A matriz deve ser 13x11
+    map = np.matrix([['GR', 'GR', 'GR', 'BO', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR'], 
+                     ['GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR'],
+                     ['GR', 'GR', 'BO', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR'], 
+                     ['GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR'],
+                     ['GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR'], 
+                     ['GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR'],
+                     ['GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR'], 
+                     ['GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR'],
+                     ['GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR'], 
+                     ['GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR', 'BA', 'GR'],
+                     ['GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR', 'GR']])
 
     pygame.init()
-    start = StartGame(map)
-
-
+    start = StartGame(map, theme='classic')
 
